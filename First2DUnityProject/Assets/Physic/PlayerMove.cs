d6bdbca54c58dce42c3b2ae1b6d81ae1b2970256
hangooksaram <hyunjae1995@gmail.com>
2022-01-19 23:55:17 +0900

    fix:AddForce 버그 수정

--- First2DUnityProject/Assets/Physic/PlayerMove.cs
+++ First2DUnityProject/Assets/Physic/PlayerMove.cs
@@ -11,19 +11,18 @@ public class PlayerMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();    
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         // Move ( Left Arrow & Right Arrow )
         float horizontal = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * horizontal, ForceMode2D.Impulse);
-        if (rigid.velocity.x > maxSpeed) // Right Max Speed
-        {
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        }
-        else if (rigid.velocity.x < maxSpeed * (-1))
-        {
-            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-        }
+        //if (rigid.velocity.x > maxSpeed) // Right Max Speed
+        //{
+        //    rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        //}
+        //else if (rigid.velocity.x < maxSpeed * (-1))
+        //{
+        //    rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        //}
     }
 }
