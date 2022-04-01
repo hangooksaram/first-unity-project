c64d3f651b76875ad94df85236a864ca17ea2f05
hangooksaram <hyunjae1995@gmail.com>
2022-04-02 01:03:53 +0900

    fix:유니티 vs 연동 버그 수정 및 아이템 Move 추가

--- First2DUnityProject/Assets/Move/ItemMove.cs
+++ First2DUnityProject/Assets/Move/ItemMove.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class ItemMove : MonoBehaviour
 {
-    void Start()
+    Rigidbody2D rigid;
+    public float itemNextMove;
+    public float nextMoveTime;
+    void Awake()
     {
+        rigid = GetComponent<Rigidbody2D>();
+        itemNextMove = 0.5f;
+        Move();
     }
 
-    void FixedAndUpdate()
+    void FixedUpdate()
     {
-        
+    }
+    
+    public void Move()
+    {
+        Debug.Log(itemNextMove);
+        itemNextMove *= -1f;
+        rigid.velocity = new Vector2(0, itemNextMove);
+        Invoke("Move", 0.5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+
+        }
     }
 }
